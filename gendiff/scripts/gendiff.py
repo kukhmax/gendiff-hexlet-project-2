06836b28c4bfda5702acc93adfa98a8afc2033a8
kukhmax <kukhmax@gmail.com>
2021-09-08 17:54:40 +0200

    modified gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,9 +2,13 @@
 
 import argparse
 
+#positional arguments
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
+
+#named arguments
+parser.add_argument('-f','--format', help='set format of output') 
 args = parser.parse_args()
 print(args)
 
