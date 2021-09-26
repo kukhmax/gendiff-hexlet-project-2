ac886fead41a9a18590c6fd33c8d6b7201df5224
kukhmax <kukhmax@gmail.com>
2021-09-26 14:09:51 +0200

    fixed import generate_diff()

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,5 +1,7 @@
 from gendiff.formatters.formatter import generate_diff
+import yaml
 
 __all__ = (  # noqa: WPS410
     'generate_diff',
+    'yaml'
 )
--- gendiff/formatters/parser_data.py
+++ gendiff/formatters/parser_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import json
-import yaml
+from json import load
+from yaml import safe_load
 from typing import Dict, Tuple
 
 
@@ -15,12 +15,12 @@ def get_parsed_files(path_file1: str, path_file2: str) -> Tuple[Dict, Dict]:
     """
     if path_file1[-4:] == ".yml" or path_file1[-5:] == ".yaml":
         with open(path_file1) as f1:
-            dict_from_file1 = yaml.safe_load(f1)
+            dict_from_file1 = safe_load(f1)
         with open(path_file2) as f2:
-            dict_from_file2 = yaml.safe_load(f2)
+            dict_from_file2 = safe_load(f2)
     elif path_file1[-5:] == ".json":
         with open(path_file1) as f1:
-            dict_from_file1 = json.load(f1)
+            dict_from_file1 = load(f1)
         with open(path_file2) as f2:
-            dict_from_file2 = json.load(f2)
+            dict_from_file2 = load(f2)
     return dict_from_file1, dict_from_file2
