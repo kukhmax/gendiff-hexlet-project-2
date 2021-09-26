b5c6632983eb62f5adbdf714ef27634e2a23e2c2
kukhmax <kukhmax@gmail.com>
2021-09-26 13:24:03 +0200

    fixed import generate_diff()

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,3 +1,3 @@
-from gendiff.formatters.formatter import generate_diff
-import yaml
-import json
+from gendiff.formatters.formatter import generate_diff  # noqa F401
+from gendiff.formatters.formatter import yaml  # noqa F401
+from gendiff.formatters.formatter import json  # noqa F401
--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -7,4 +7,4 @@ from gendiff.formatters.formatter import generate_diff
 def get_json(path_file1: str, path_file2: str) -> str:
     """Serialize 'diff' to a JSON formatted 'str' """
     diff = generate_diff(path_file1, path_file2)
-    return dumps(diff, indent=2)
+    return dumps(diff)
